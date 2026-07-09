6953ddaf8877a056188c6a67cc6ad9560844349e
Val's Revision <godsongovina56@gmail.com>
2026-07-09 19:24:02 +0100

    Create data-mathematics.js

---
+++ data-mathematics.js
@@ -0,0 +1,27 @@
+const mathematicsData = {
+  "101": [
+    {name: "Algebra", questions: [
+      {q:"Solve: 2x + 4 = 10", options:["2","3","4","5"], a:"3"},
+      {q:"Factorize: x² - 9", options:["(x-3)(x+3)","(x-9)(x+1)","(x-3)²","x(x-9)"], a:"(x-3)(x+3)"},
+      {q:"Solve: 3y - 7 = 8", options:["3","4","5","6"], a:"5"},
+      {q:"Expand: (x + 2)(x - 2)", options:["x²-4","x²+4","x²-2","x²+2"], a:"x²-4"},
+      {q:"If x=2, what is 3x²?", options:["6","9","12","15"], a:"12"}
+    ]},
+    {name: "Calculus", questions: [
+      {q:"Derivative of x²?", options:["x","2x","x²","2"], a:"2x"},
+      {q:"Integral of 2x dx?", options:["x²+C","2x²+C","x+C","2x+C"], a:"x²+C"},
+      {q:"Derivative of 5?", options:["0","1","5","x"], a:"0"},
+      {q:"Limit of x as x->2?", options:["0","1","2","4"], a:"2"},
+      {q:"Derivative of sinx?", options:["cosx","-sinx","-cosx","tanx"], a:"cosx"}
+    ]}
+  ],
+  "102": [
+    {name: "Statistics", questions: [
+      {q:"Average of 2,4,6?", options:["3","4","5","6"], a:"4"},
+      {q:"Mode of 1,2,2,3,4?", options:["1","2","3","4"], a:"2"},
+      {q:"Median of 1,3,5,7,9?", options:["3","5","7","9"], a:"5"},
+      {q:"Probability of getting Head?", options:["0","0.5","1","2"], a:"0.5"},
+      {q:"Range of 2,5,8,1,9?", options:["7","8","9","10"], a:"8"}
+    ]}
+  ]
+};
