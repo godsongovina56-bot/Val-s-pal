8d1d4d3118f02fbe041d7d728a6766a35f4cb8e3
Val's Revision <godsongovina56@gmail.com>
2026-07-09 20:25:23 +0100

    Add biology data with questions and answers

---
+++ data-biology.js
@@ -0,0 +1,27 @@
+const biologyData = {
+  "101": [
+    {name: "Cell Biology", questions: [
+      {q:"Powerhouse of the cell?", options:["Nucleus","Mitochondria","Ribosome","Chloroplast"], a:"Mitochondria"},
+      {q:"DNA is found in?", options:["Cytoplasm","Nucleus","Ribosome","Mitochondria"], a:"Nucleus"},
+      {q:"Plant cell wall is made of?", options:["Protein","Cellulose","Lipid","Chitin"], a:"Cellulose"},
+      {q:"Protein synthesis occurs in?", options:["Nucleus","Ribosome","Golgi","Lysosome"], a:"Ribosome"},
+      {q:"Cell membrane is?", options:["Permeable","Semi-permeable","Impermeable","None"], a:"Semi-permeable"}
+    ]},
+    {name: "Genetics", questions: [
+      {q:"Father of Genetics?", options:["Darwin","Mendel","Pasteur","Watson"], a:"Mendel"},
+      {q:"DNA stands for?", options:["Deoxyribonucleic Acid","Dinucleic Acid","Double Nucleic Acid","None"], a:"Deoxyribonucleic Acid"},
+      {q:"Number of chromosomes in human?", options:["23","46","44","48"], a:"46"},
+      {q:"Sex chromosomes?", options:["XX,XY","AA,BB","XY,YY","XX,YY"], a:"XX,XY"},
+      {q:"Gene is located in?", options:["Chromosome","Cell","Nucleus","Cytoplasm"], a:"Chromosome"}
+    ]}
+  ],
+  "102": [
+    {name: "Ecology", questions: [
+      {q:"Study of environment?", options:["Biology","Ecology","Zoology","Botany"], a:"Ecology"},
+      {q:"Food chain starts with?", options:["Consumer","Producer","Decomposer","Herbivore"], a:"Producer"},
+      {q:"Ozone layer protects from?", options:["UV rays","Rain","Wind","Heat"], a:"UV rays"},
+      {q:"Biotic factor?", options:["Water","Soil","Plant","Air"], a:"Plant"},
+      {q:"Ecosystem includes?", options:["Living only","Non-living only","Both","None"], a:"Both"}
+    ]}
+  ]
+};
