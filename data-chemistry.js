f05367f8414b48b944f892b8e748e63749013944
Val's Revision <godsongovina56@gmail.com>
2026-07-09 20:19:55 +0100

    Create data-chemistry.js

---
+++ data-chemistry.js
@@ -0,0 +1,27 @@
+const chemistryData = {
+  "101": [
+    {name: "Atomic Structure", questions: [
+      {q:"Number of protons in an atom?", options:["Atomic Mass","Atomic Number","Valency","Mole"], a:"Atomic Number"},
+      {q:"Electron is in?", options:["Nucleus","Shell","Orbit","All of the above"], a:"Shell"},
+      {q:"Neutron has?", options:["+1 charge","-1 charge","No charge","+2 charge"], a:"No charge"},
+      {q:"Atomic mass =?", options:["p+n","p+e","n+e","p+n+e"], a:"p+n"},
+      {q:"Isotopes have same?", options:["Mass","Protons","Neutrons","Electrons"], a:"Protons"}
+    ]},
+    {name: "Chemical Bonding", questions: [
+      {q:"Bond in NaCl?", options:["Covalent","Ionic","Metallic","Hydrogen"], a:"Ionic"},
+      {q:"Bond in H2O?", options:["Covalent","Ionic","Metallic","Hydrogen"], a:"Covalent"},
+      {q:"Sharing of electrons?", options:["Ionic","Covalent","Metallic","Van der Waals"], a:"Covalent"},
+      {q:"Transfer of electrons?", options:["Ionic","Covalent","Metallic","Coordinate"], a:"Ionic"},
+      {q:"Diamond has?", options:["Ionic","Covalent","Metallic","Hydrogen"], a:"Covalent"}
+    ]}
+  ],
+  "102": [
+    {name: "Organic Chemistry", questions: [
+      {q:"Functional group of Alcohol?", options:["-COOH","-OH","-CHO","-NH2"], a:"-OH"},
+      {q:"Alkane general formula?", options:["CnH2n","CnH2n+2","CnH2n-2","CnHn"], a:"CnH2n+2"},
+      {q:"Ethanol formula?", options:["CH3OH","C2H5OH","C3H7OH","C4H9OH"], a:"C2H5OH"},
+      {q:"Carboxylic acid group?", options:["-OH","-COOH","-CHO","-CO"], a:"-COOH"},
+      {q:"Methane is?", options:["Alkane","Alkene","Alkyne","Alcohol"], a:"Alkane"}
+    ]}
+  ]
+};
